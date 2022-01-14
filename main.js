91ab05eed6f7e9b32393b4f3c74bff3ed81952c6
BabelWebDeveloper <babel.web.developer@gmail.com>
2022-01-14 19:22:06 +0100

    init

--- main.js
+++ main.js
@@ -2,50 +2,85 @@ let matchScreen = window.matchMedia("(max-width: 768px)")
 let getMenuBtn = document.getElementById('menuButtonMobile')
 let getUl = document.getElementById('menu')
 let getNav = document.getElementById('navi')
-let menuOpen = false
+let menuStatus = false
 
-let setScreenOnLoad = () => {
-    menuOpen = false
-    if(matchScreen.matches) {
-        console.log(menuOpen)
-        getMenuBtn.addEventListener('click', () => {
-            if(menuOpen === false) {
-                getMenuBtn.classList.add('open')
-                getUl.style.display = 'flex'
-                getNav.style.background = 'rgba(0, 0, 0, 0.514)'
-                menuOpen = true
-                console.log(menuOpen)
-            } else {
-                getMenuBtn.classList.remove('open')
-                getUl.style.display = 'none'
-                getNav.style.background = 'transparent'
-                menuOpen = false
-                console.log(menuOpen)
-            }
-        })
+// Check screen start here:
+let checkScreen = (screen) => {
+    let value = ''
+    if (matchScreen.matches) {
+        value = 'Small'
     } else {
-        getUl.style.display = 'flex'
-        console.log(menuOpen)
+        value = 'Big'
+    }
+    screen(value);
+}
+// Check screen end here.
 
+// Display show start here:
+let displayShow = (menuDisplay, getUlDisplay, getNavBackground) => {
+    getMenuBtn.style.display = menuDisplay
+    getUl.style.display = getUlDisplay
+    getNav.style.background = getNavBackground
+}
+// Display show end here.
+
+// Display menu status start here:
+let displayStatusMenu = (status) => {
+    menuStatus = status
+}
+// Display menu status end here.
+
+// Screen on load/on change start here:
+let setScreen = (formCheckScreen) => {
+    if (formCheckScreen === 'Small') {
+        displayShow('inline','none','transparent')
+        getMenuBtn.classList.remove('open')
+        displayStatusMenu(false)
+        console.log(menuStatus)
+    } else if (formCheckScreen === 'Big'){
+        displayShow('none','flex','black')
+        getMenuBtn.classList.remove('open')
+        displayStatusMenu(false)
+        console.log(menuStatus)
     }
 }
+let screenOnLoad = () => {
+    checkScreen(setScreen)
+} 
+matchScreen.addEventListener('change', screenOnLoad)
+window.addEventListener('DOMContentLoaded', screenOnLoad)
+// Screen on load/on change end here.
 
-let setScreenOnChange = () => {
-    if(matchScreen.matches){
-        menuOpen = false
-        getMenuBtn.classList.remove('open')
-        getUl.style.display = 'none'
-        getNav.style.background = 'transparent'
-        console.log(menuOpen)
-    } else {
-        menuOpen = true
-        getMenuBtn.classList.remove('open')
-        getUl.style.display = 'flex'
-        getNav.style.background = 'black'
-        console.log(menuOpen)
+// List chapter control start here:
+let listChapter = (formCheckScreen) => {
+    if (formCheckScreen === 'Small') {
+        displayShow('inline','none')
+        if (menuStatus === true) {
+            displayShow('inline','none','transparent')
+            getMenuBtn.classList.remove('open')
+            displayStatusMenu(false)
+            console.log(menuStatus)
+        } else if (menuStatus === false) {
+            displayShow('inline','flex','rgba(0,0,0,0.25')
+            getMenuBtn.classList.add('open')
+            displayStatusMenu(true)
+            console.log(menuStatus)
+        }
+    } else if (formCheckScreen === 'Big'){
+        displayShow('none','flex')
+        if (menuStatus === true) {
+            displayShow('none','flex','black')
+            displayStatusMenu(false)
+            console.log(menuStatus)
+        } else if (menuStatus === false) {
+            displayShow('none','flex','black')
+            displayStatusMenu(true)
+            console.log(menuStatus)
+        }   
     }
-    
 }
-
-matchScreen.addEventListener('change', setScreenOnChange)
-window.addEventListener('DOMContentLoaded', setScreenOnLoad)
+let listChapterControl = () => {
+    checkScreen(listChapter)
+} 
+getMenuBtn.addEventListener('click', listChapterControl)
+// List chapter control end here.
